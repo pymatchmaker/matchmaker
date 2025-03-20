fix get_ppq & transfer position to causal nearest

matchmaker/matchmaker.py
@@ -19,7 +19,7 @@ from matchmaker.features.midi import PianoRollProcessor, PitchIOIProcessor
 from matchmaker.io.audio import AudioStream
 from matchmaker.io.midi import MidiStream
 from matchmaker.prob.hmm import PitchIOIHMM
-from matchmaker.utils.eval import TOLERANCES, get_evaluation_results
+from matchmaker.utils.eval import TOLERANCES, get_evaluation_results, transfer_positions
 from matchmaker.utils.misc import (
     adjust_tempo_for_performance_audio,
     generate_score_audio,
@@ -260,6 +260,7 @@ class Matchmaker(object):
     def build_score_annotations(self, level="beat"):
         score_annots = []
         if level == "beat":  # TODO: add bar-level, note-level
+            # self.score_part.use_musical_beat()  # for asap
             note_array = np.unique(self.score_part.note_array()["onset_beat"])
             start_beat = np.ceil(note_array.min())
             end_beat = np.floor(note_array.max())
@@ -267,7 +268,9 @@ class Matchmaker(object):
 
             beat_timestamp = [
                 self.score_part.inv_beat_map(beat)
-                / get_ppq(self.score_part)
+                / self.score_part.quarter_duration_map(
+                    self.score_part.inv_beat_map(beat)
+                )
                 * (60 / self.tempo)
                 for beat in beats
             ]
@@ -308,12 +311,14 @@ class Matchmaker(object):
         score_annots = self.build_score_annotations(level)
         perf_annots = np.loadtxt(fname=perf_annotations, delimiter="\t", usecols=0)
 
-        # print(f"score annotations: {score_annots}, len: {len(score_annots)}")
-        # print(f"performance annotations: {perf_annots}, len: {len(perf_annots)}")
-
         min_length = min(len(score_annots), len(perf_annots))
         score_annots = score_annots[:min_length]
         perf_annots = perf_annots[:min_length]
+
+        perf_annots_predicted = transfer_positions(
+            self.score_follower.warping_path, score_annots, frame_rate=self.frame_rate
+        )
+        perf_annots = perf_annots[: len(perf_annots_predicted)]
 
         if debug:
             # save score audio with beat annotations
@@ -334,11 +339,20 @@ class Matchmaker(object):
                 save_path=perf_audio_dir
                 / f"perf_audio_{Path(self.performance_file).parent.parent.name}_{Path(self.performance_file).parent.name}_{Path(self.performance_file).stem}.wav",
             )
+            # save performance audio with predicted beat annotations
+            perf_predicted_audio_dir = Path("./performance_audio_predicted")
+            perf_predicted_audio_dir.mkdir(parents=True, exist_ok=True)
+            save_mixed_audio(
+                self.performance_file,
+                perf_annots_predicted,
+                save_path=perf_predicted_audio_dir
+                / f"perf_audio_{Path(self.performance_file).parent.parent.name}_{Path(self.performance_file).parent.name}_{Path(self.performance_file).stem}.wav",
+            )
 
         return get_evaluation_results(
             score_annots,
             perf_annots,
-            self.score_follower.warping_path,
+            perf_annots_predicted,
             self.frame_rate,
             tolerances,
         )

matchmaker/utils/eval.py
@@ -21,31 +21,60 @@ def transfer_positions(wp, ref_anns, frame_rate):
         warping_path[0] is the index of the reference (score) feature and warping_path[1] is the index of the target(input) feature.
     ref_ann : List[float]
         reference annotations in seconds.
+    frame_rate : int
+        frame rate of the audio.
+
+    Returns
+    -------
+    predicted_targets : np.array with shape (T,)
+        predicted target positions in seconds.
     """
+    # Causal nearest interpolation
     x, y = wp[0], wp[1]
     ref_anns_frame = np.round(ref_anns * frame_rate)
-    predicted_targets = np.array(
-        [
-            y[np.where(x >= r)[0][0]]
-            for r in ref_anns_frame
-            if np.where(x >= r)[0].size > 0
-        ]
-    )
-    return predicted_targets / frame_rate
+    predicted_targets = []
+
+    for r in ref_anns_frame:
+        past_indices = np.where(x <= r)[0]
+        if past_indices.size > 0:
+            nearest_past_idx = past_indices[-1]
+            predicted_targets.append(y[nearest_past_idx])
+
+    return np.array(predicted_targets) / frame_rate
+
+    # 1. nearest interpolation
+    # ref_anns_frame = np.round(ref_anns * frame_rate)
+    # positions_1_transferred_to_2 = scipy.interpolate.interp1d(
+    #     wp[0], wp[1], kind="nearest"
+    # )(ref_anns_frame)
+    # return positions_1_transferred_to_2 / frame_rate
+
+    # 2. threshold-crossing
+    # x, y = wp[0], wp[1]
+    # ref_anns_frame = np.round(ref_anns * frame_rate)
+    # predicted_targets = np.array(
+    #     [
+    #         y[np.where(x >= r)[0][0]]
+    #         for r in ref_anns_frame
+    #         if np.where(x >= r)[0].size > 0
+    #     ]
+    # )
+    # return predicted_targets / frame_rate
 
 
 def get_evaluation_results(
     score_annots,
     perf_annots,
-    warping_path,
+    perf_annots_predicted,
+    # warping_path,
     frame_rate,
     tolerances,
 ):
-    target_annots_predicted = transfer_positions(
-        warping_path, score_annots, frame_rate=frame_rate
-    )
-    perf_annots = perf_annots[: len(target_annots_predicted)]
-    errors_in_delay = (perf_annots - target_annots_predicted) * 1000  # in milliseconds
+    # target_annots_predicted = transfer_positions(
+    #     warping_path, score_annots, frame_rate=frame_rate
+    # )
+    # perf_annots = perf_annots[: len(target_annots_predicted)]
+    errors_in_delay = (perf_annots - perf_annots_predicted) * 1000  # in milliseconds
 
     absolute_errors_in_delay = np.abs(errors_in_delay)
     filtered_abs_errors_in_delay = absolute_errors_in_delay[
@@ -96,7 +125,7 @@ def save_score_following_result(
     ref_paths, target_paths = model.warping_path[0], model.warping_path[1]
     for n in range(len(ref_paths)):
         plt.plot(
-            target_paths[n], ref_paths[n], ".", color="purple", alpha=0.5, markersize=3
+            target_paths[n], ref_paths[n], ".", color="lime", alpha=0.5, markersize=3
         )
 
     # plot ground-truth labels
@@ -105,6 +134,6 @@ def save_score_following_result(
         perf_annots = [float(row[0]) for row in reader]
     for i, (ref, target) in enumerate(zip(score_annots, perf_annots)):
         plt.plot(
-            target * frame_rate, ref * frame_rate, "x", color="r", alpha=1, markersize=3
+            target * frame_rate, ref * frame_rate, "x", color="r", alpha=1, markersize=5
         )
     plt.savefig(save_dir / f"{run_name}.png")