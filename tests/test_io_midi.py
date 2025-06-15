b85980cc18969a18c2dbdc05f5c197cf16c69b20
jiyun.park <laurenceyoon@gmail.com>
2025-06-15 15:05:18 +0900

    fix broken tests w/ i/o midi

--- tests/test_io_midi.py
+++ tests/test_io_midi.py
@@ -3,32 +3,41 @@
 """
 Tests for the io/midi.py module
 """
-import time
+
 import unittest
-from io import StringIO
+from tempfile import NamedTemporaryFile
 from typing import Optional
-from unittest.mock import patch
 
 import mido
 import numpy as np
 import partitura as pt
+from partitura import save_performance_midi
+from partitura.performance import PerformedPart
 
 from matchmaker import EXAMPLE_PERFORMANCE
 from matchmaker.features.midi import PianoRollProcessor, PitchIOIProcessor
 from matchmaker.io.mediator import CeusMediator
-from matchmaker.io.midi import Buffer, MidiStream
+from matchmaker.io.midi import MidiStream
 from matchmaker.utils.misc import RECVQueue
-from matchmaker.utils.processor import DummyProcessor
-from matchmaker.utils.symbolic import midi_messages_from_midi, panic_button
+from matchmaker.utils.symbolic import midi_messages_from_midi
+from tests.utils import DummyMidiPlayer
 
 RNG = np.random.RandomState(1984)
 
-from tempfile import NamedTemporaryFile
 
-from partitura import save_performance_midi
-from partitura.performance import PerformedPart
+def has_midi_devices():
+    """Check if system has MIDI devices"""
+    try:
+        DEVICE_COUNT = len(mido.get_input_names())
+        has_midi = DEVICE_COUNT > 0
+    except Exception:
+        has_midi = False
 
-from tests.utils import DummyMidiPlayer
+    return has_midi
+
+
+HAS_MIDI_DEVICES = has_midi_devices()
+SKIP_REASON = (not HAS_MIDI_DEVICES, "No MIDI devices detected")
 
 
 def setup_midi_player(use_example: bool = False):
@@ -157,6 +166,7 @@ class TestMidiStream(unittest.TestCase):
             virtual_port=virtual_port,
         )
 
+    @unittest.skipIf(*SKIP_REASON)
     def test_init(self):
         """Test that the MidiStream initializes correctly"""
         for processor in [
@@ -195,8 +205,8 @@ class TestMidiStream(unittest.TestCase):
                             if port is not None:
                                 port.close()
 
+    @unittest.skipIf(*SKIP_REASON)
     def test_init_port_selection(self):
-
         # Raise an error if port is incorrect
         with self.assertRaises(ValueError):
             self.setup(port="wrong_port")
@@ -221,6 +231,7 @@ class TestMidiStream(unittest.TestCase):
         self.assertTrue(self.stream.midi_in is None)
 
     # @patch("sys.stdout", new_callable=StringIO)
+    @unittest.skipIf(*SKIP_REASON)
     def test_run_online(self, mock_stdout=None):
         """
         Test running an instance of a MidiStream class
@@ -231,7 +242,6 @@ class TestMidiStream(unittest.TestCase):
             for return_midi_messages in [True, False]:
                 for use_mediator in [True, False]:
                     for polling_period in [None, 0.01]:
-
                         port, queue, midi_player, _, mediator = setup_midi_player()
 
                         self.setup(
@@ -278,6 +288,7 @@ class TestMidiStream(unittest.TestCase):
                         port.close()
 
     # @patch("sys.stdout", new_callable=StringIO)
+    @unittest.skipIf(*SKIP_REASON)
     def test_run_online_context_manager(self, mock_stdout=None):
         """
         Test running an instance of a MidiStream class
@@ -303,7 +314,6 @@ class TestMidiStream(unittest.TestCase):
         )
 
         with self.stream as stream:
-
             midi_player.start()
 
             while midi_player.is_playing:
@@ -350,7 +360,6 @@ class TestMidiStream(unittest.TestCase):
             outputs = list(self.stream.queue.queue)
             self.assertTrue(len(outputs) == len(valid_messages))
 
-    # @patch("sys.stdout", new_callable=StringIO)
     def test_run_offline_windowed(self, mock_stdout=None):
         """
         Test run_offline_windowed method.
@@ -381,7 +390,6 @@ class TestMidiStream(unittest.TestCase):
 
             self.assertTrue(len(outputs) == expected_frames)
 
-    # @patch("sys.stdout", new_callable=StringIO)
     def test_clear_queue(self, mock_stdout=None):
         """
         Test clear_queue method
@@ -401,7 +409,7 @@ class TestMidiStream(unittest.TestCase):
         outputs = list(self.stream.queue.queue)
         self.assertTrue(len(outputs) == 0)
 
-    # @patch("sys.stdout", new_callable=StringIO)
+    @unittest.skipIf(*SKIP_REASON)
     def test_online_windowed_input(self, mock_stdout=None):
         port, queue, midi_player, note_array, _ = setup_midi_player()
 
